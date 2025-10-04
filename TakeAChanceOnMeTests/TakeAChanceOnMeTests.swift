5cf3868d5f8f17137c6d099888391548af5bfc3b
Michael Fluharty <michael.fluharty@mac.com>
2025-10-03 19:37:34 -0500

    Initial Commit

---
+++ TakeAChanceOnMeTests/TakeAChanceOnMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeAChanceOnMeTests.swift
+//  TakeAChanceOnMeTests
+//
+//  Created by Michael Fluharty on 10/3/25.
+//
+
+import Testing
+@testable import TakeAChanceOnMe
+
+struct TakeAChanceOnMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
